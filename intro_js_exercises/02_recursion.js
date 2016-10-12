e401d4b1034781044f75d26b13886e4b422da745
Elizabeth Sparks <erhsparks@gmail.com>
2016-10-12 15:27:04 -0700

    Intro JS exercises, recursion II

--- intro_js_exercises/02_recursion.js
+++ intro_js_exercises/02_recursion.js
@@ -58,7 +58,7 @@ function exp2(base, exponent) {
 function finbonacciR(num) {
   if (num === 1) return [1];
   if (num === 2) return [1, 1];
-  if (num <= 0) return NaN;
+  if (num <= 0) return undefined;
 
   let last = finbonacciR(num - 1);
   return last.concat(last[last.length-2] + last[last.length-1]);
@@ -81,6 +81,69 @@ function finbonacciI(num) {
   return fibs;
 }
 
-console.log(finbonacciI(1));
-console.log(finbonacciI(2));
-console.log(finbonacciI(7));
+// console.log(finbonacciI(1));
+// console.log(finbonacciI(2));
+// console.log(finbonacciI(7));
+
+
+function bSearch(arr, target) {
+  if (arr.length <= 0) return undefined;
+
+  let half = Math.floor(arr.length/2);
+
+  switch (Math.sign(target - arr[half])) {
+  case -1:
+    return bSearch(arr.slice(0, half), target);
+  case 0:
+    return half;
+  case 1:
+    let right = bSearch(arr.slice(half+1, arr.length), target);
+    if (right === undefined) {
+      return right;
+    } else {
+      return half + 1 + right;
+    }
+
+  }
+}
+//
+// console.log(bSearch([1, 2, 3], 1));
+// console.log(bSearch([2, 3, 4, 5], 3));
+// console.log(bSearch([2, 4, 6, 8, 10], 6));
+// console.log(bSearch([1, 3, 4, 5, 9], 5));
+// console.log(bSearch([1, 2, 3, 4, 5, 6], 6));
+// console.log(bSearch([1, 2, 3, 4, 5, 6], 0));
+// console.log(bSearch([1, 2, 3, 4, 5, 7], 6));
+
+
+function mergeSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  let half = Math.floor(arr.length/2);
+
+  let left = mergeSort(arr.slice(0, half));
+  let right = mergeSort(arr.slice(half, arr.length));
+
+  return merge(left, right);
+}
+
+function merge(arr1, arr2) {
+  let result = [];
+
+  while ((arr1.length > 0) && (arr2.length > 0)) {
+    if (arr1[0] <= arr2[0]) {
+      result.push(arr1.shift());
+    } else {
+      result.push(arr2.shift());
+    }
+  }
+  return result.concat(arr1).concat(arr2);
+}
+
+console.log(mergeSort([6,2,34,6,7,23,4,87]));
+console.log(mergeSort([6,2]));
+
+
+function subsets() {
+
+}
